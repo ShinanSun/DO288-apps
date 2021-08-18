0099e6b41574ac504ffeab98d01bc9115313d6ae
Student User <student@workstation.lab.example.com>
2021-08-17 23:32:49 -0400

    work

--- quotes/index.php
+++ quotes/index.php
@@ -1,3 +1,15 @@
 <?php
-    print "This app returns a random funny quote.\n"
+    print "This app returns a random funny quote.\n "  
 ?>
+
+ <h1>
+    <?php
+        echo 'My OCP is ' .$_ENV["MY_OCP_DEVOPS"] . '!';
+     ?>
+    <?php
+        echo 'My EXAM is ' .$_ENV["MY_EXAM"] . '!';
+     ?>
+    <?php
+        echo 'My EXAM is ' .$_ENV["AUDIENCE"] . '!';
+     ?>
+ </h1>
